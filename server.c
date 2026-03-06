c5cb53345705fec3333b5d3ee3f5e4670379bff9
Gojus1 <gojus.kastenas123@gmail.com>
2026-03-06 19:02:00 +0200

    simple enigma added

--- server.c
+++ server.c
@@ -3,11 +3,34 @@
 #include <string.h>
 #include <microhttpd.h>
 
-/* cipher backends */
 #include "internals/cyphers/rabin.h"
-#include "internals/cyphers/rsa.h"
+#include "internals/cyphers/enigma.h"
 
-#define PORT_DEFAULT 8080
+#define PORT 8080
+
+static char* safe_strdup(const char* s)
+{
+    if(!s) return NULL;
+    char* r = malloc(strlen(s)+1);
+    if(r) strcpy(r,s);
+    return r;
+}
+
+static int send_response(struct MHD_Connection *connection, const char *text)
+{
+    struct MHD_Response *response =
+        MHD_create_response_from_buffer(
+            strlen(text),
+            (void*)text,
+            MHD_RESPMEM_MUST_COPY);
+
+    MHD_add_response_header(response,"Content-Type","text/plain; charset=utf-8");
+    MHD_add_response_header(response,"Access-Control-Allow-Origin","*");
+
+    int ret = MHD_queue_response(connection,200,response);
+    MHD_destroy_response(response);
+    return ret;
+}
 
 static int handle_request(
     void *cls,
@@ -17,113 +40,79 @@ static int handle_request(
     const char *version,
     const char *upload_data,
     size_t *upload_data_size,
-    void **con_cls
-) {
+    void **con_cls)
+{
     (void)cls;
     (void)version;
     (void)upload_data;
     (void)upload_data_size;
     (void)con_cls;
 
-    if (strcmp(method, "GET") != 0) {
+    if(strcmp(method,"GET")!=0)
         return MHD_NO;
-    }
 
-    /* query parameters */
-    const char *alph   = MHD_lookup_connection_value(connection, MHD_GET_ARGUMENT_KIND, "alph");
-    const char *cipher = MHD_lookup_connection_value(connection, MHD_GET_ARGUMENT_KIND, "cipher");
-    const char *frag   = MHD_lookup_connection_value(connection, MHD_GET_ARGUMENT_KIND, "frag");
+    const char *alph   = MHD_lookup_connection_value(connection,MHD_GET_ARGUMENT_KIND,"alph");
+    const char *cipher = MHD_lookup_connection_value(connection,MHD_GET_ARGUMENT_KIND,"cipher");
+    const char *frag   = MHD_lookup_connection_value(connection,MHD_GET_ARGUMENT_KIND,"frag");
 
-    if (!frag) {
-        const char *msg = "ERROR: missing frag\n";
-        struct MHD_Response *res =
-            MHD_create_response_from_buffer(
-                strlen(msg),
-                (void*)msg,
-                MHD_RESPMEM_PERSISTENT
-            );
-        int ret = MHD_queue_response(connection, 400, res);
-        MHD_destroy_response(res);
-        return ret;
-    }
+    char *alph_c   = safe_strdup(alph);
+    char *cipher_c = safe_strdup(cipher);
+    char *frag_c   = safe_strdup(frag);
+
+    if(!cipher_c || !frag_c)
+        return send_response(connection,"ERROR: missing cipher or frag\n");
 
     const char *result = NULL;
 
-    /* ROUTING */
-    if (strcmp(url, "/rabin") == 0) {
-        result = rabinEntry(alph, cipher, frag);
+    if(strcmp(url,"/rabin")==0)
+    {
+        result = rabinEntry(alph_c,cipher_c,frag_c);
     }
-    else if (strcmp(url, "/rsa") == 0) {
-        result = rsaEntry(alph, cipher, frag);
+    else if(strcmp(url,"/enigma")==0)
+    {
+        result = enigmaEntry(alph_c,cipher_c,frag_c);
     }
-    else {
-        const char *msg = "404 Not Found\n";
-        struct MHD_Response *res =
-            MHD_create_response_from_buffer(
-                strlen(msg),
-                (void*)msg,
-                MHD_RESPMEM_PERSISTENT
-            );
-        int ret = MHD_queue_response(connection, 404, res);
-        MHD_destroy_response(res);
-        return ret;
+    else
+    {
+        free(alph_c);
+        free(cipher_c);
+        free(frag_c);
+        return send_response(connection,"404 Not Found\n");
     }
 
-    if (!result) {
-        result = "ERROR: internal failure\n";
-    }
+    int ret = send_response(connection,result);
 
-    struct MHD_Response *response =
-        MHD_create_response_from_buffer(
-            strlen(result),
-            (void*)result,
-            MHD_RESPMEM_MUST_COPY
-        );
-
-    MHD_add_response_header(response, "Content-Type", "text/plain; charset=utf-8");
-    MHD_add_response_header(response, "Access-Control-Allow-Origin", "*");
-
-    int ret = MHD_queue_response(connection, 200, response);
-    MHD_destroy_response(response);
-
-    /* IMPORTANT:
-       DO NOT free(result)
-       Rabin/RSA manage their own internal buffers
-    */
+    free((void*)result);
+    free(alph_c);
+    free(cipher_c);
+    free(frag_c);
 
     return ret;
 }
 
-int main(int argc, char **argv) {
-    int port = PORT_DEFAULT;
+int main()
+{
+    struct MHD_Daemon *daemon;
 
-    if (argc > 1) {
-        port = atoi(argv[1]);
-        if (port <= 0) port = PORT_DEFAULT;
-    }
-
-    struct MHD_Daemon *daemon = MHD_start_daemon(
+    daemon = MHD_start_daemon(
         MHD_USE_SELECT_INTERNALLY,
-        port,
+        PORT,
         NULL,
         NULL,
         &handle_request,
         NULL,
-        MHD_OPTION_END
-    );
+        MHD_OPTION_END);
 
-    if (!daemon) {
-        fprintf(stderr, "Failed to start server\n");
+    if(!daemon)
+    {
+        printf("Failed to start server\n");
         return 1;
     }
 
-    printf("=====================================\n");
-    printf(" Sabaton Crypto HTTP Server\n");
-    printf("=====================================\n");
-    printf("Running on: http://localhost:%d\n", port);
+    printf("Server running at http://localhost:%d\n",PORT);
     printf("Endpoints:\n");
-    printf("  GET /rabin\n");
-    printf("  GET /rsa\n");
+    printf("  /rabin\n");
+    printf("  /enigma\n");
     printf("\nPress ENTER to stop.\n");
 
     getchar();
@@ -131,4 +120,3 @@ int main(int argc, char **argv) {
     MHD_stop_daemon(daemon);
     return 0;
 }
-//gcc ./server.c ./internals/hash.c ./internals/cyphers/affineCaesar.c ./internals/cyphers/enigma.c ./internals/cyphers/aes.c ./internals/cyphers/feistel.c ./internals/cyphers/block.c ./internals/cyphers/hill.c ./internals/cyphers/scytale.c ./internals/cyphers/transposition.c ./internals/cyphers/vigenere.c ./internals/hashes/crc32.c ./internals/hashes/murmur3.c ./internals/hashes/sha1.c ./internals/hashes/sha256.c ./internals/hashes/xxhash32.c ./internals/enhancements/lith/lithuanian.c ./internals/cyphers/bifid.c ./internals/cyphers/fleissner.c ./internals/cyphers/stream.c ./internals/cyphers/knapsack.c ./internals/cyphers/merkle.c ./internals/cyphers/graham.c ./internals/cyphers/rsa.c ./util/bigint.c ./internals/cyphers/elgamal.c ./internals/cyphers/elliptic.c ./internals/cyphers/rabin.c ./internals/cyphers/zkp.c ./internals/cyphers/shamir.c ./internals/cyphers/asmuth.c ./internals/cyphers/a5.c -lmicrohttpd -o sabaton_server
